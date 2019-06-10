a5e6ebd80a97a8cef354fb7c097e1634aa51b26c
Erik Pearson <eapearson@lbl.gov>
2019-06-10 15:24:01 -0700

    add logging output for post install

--- scripts/postinstall.bash
+++ scripts/postinstall.bash
@@ -1,6 +1,11 @@
 echo "Running plugin post install script"
 cd src/react-app && \
 npm install && \
+echo "✓ dependencies installed successfully" && \
 npm run build && \
+echo "✓ built successfully" && \
 npm run test -- --watchAll=false && \
-npm run plugin
+echo "✓ tests run successfully" && \
+npm run plugin && \
+echo "✓ plugin setup successfully" && \
+echo "✓ plugin installed successfully"
