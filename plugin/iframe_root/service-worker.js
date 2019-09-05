7792df2a8d1ff6ca952d1b71b8c3a4e484d5b217
Erik Pearson <eapearson@lbl.gov>
2019-09-05 14:40:29 -0700

    check in the build

--- plugin/iframe_root/service-worker.js
+++ plugin/iframe_root/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.93bd7d2f1fe68b3e3339d1d4da376ba6.js"
+  "./precache-manifest.30e017d4df3bf45cf462b3c51562426e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
