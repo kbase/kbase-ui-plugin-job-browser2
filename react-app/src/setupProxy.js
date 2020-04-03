59bae9cb7c25b824b4ba742b9690e7f2e40ba2e2
Erik Pearson <eapearson@lbl.gov>
2020-04-03 15:58:29 -0700

    update setupproxy

--- react-app/src/setupProxy.js
+++ react-app/src/setupProxy.js
@@ -1,16 +1,58 @@
-const proxy = require('http-proxy-middleware');
-const morgan = require('morgan');
+// const proxy = require('http-proxy-middleware');
+// const morgan = require('morgan');
+
+// module.exports = function (app) {
+//     app.use(proxy('/services/service_wizard', { target: 'http://localhost:3002', changeOrigin: true }));
+//     app.use(proxy('/services/**/*', {
+//         target: 'https://ci.kbase.us',
+//         changeOrigin: true
+//     }));
+//     app.use(proxy('/dynserv/internal/JobBrowserBFF', {
+//         target: 'http://localhost:5000',
+//         changeOrigin: true
+//     }));
+//     app.use(proxy('/dynserv/**/*', { target: 'https://ci.kbase.us', changeOrigin: true }));
+//     app.use(morgan('combined'));
+// };
+
+const {createProxyMiddleware} = require("http-proxy-middleware");
+const morgan = require("morgan");
+const DEPLOY_ENV = process.env.ENV || "ci";
+let HOST;
+
+if (DEPLOY_ENV === "prod") {
+    HOST = "kbase.us";
+} else {
+    HOST = `${DEPLOY_ENV}.kbase.us`;
+}
 
 module.exports = function (app) {
-    app.use(proxy('/services/service_wizard', { target: 'http://localhost:3002', changeOrigin: true }));
-    app.use(proxy('/services/**/*', {
-        target: 'https://ci.kbase.us',
-        changeOrigin: true
-    }));
-    app.use(proxy('/dynserv/internal/JobBrowserBFF', {
-        target: 'http://localhost:5000',
-        changeOrigin: true
-    }));
-    app.use(proxy('/dynserv/**/*', { target: 'https://ci.kbase.us', changeOrigin: true }));
-    app.use(morgan('combined'));
+    // app.use(proxy('/services/service_wizard', { target: 'http://localhost:3001', changeOrigin: true }));
+    app.use(
+        "/services/", 
+        createProxyMiddleware({
+            target: `https://${HOST}`,
+            changeOrigin: true,
+            secure: false
+        })
+    );
+    app.use(
+        "/dynserv/JobBrowserBFF", 
+        createProxyMiddleware({
+            target: `https://${HOST}`,
+            changeOrigin: true,
+            secure: false
+        })
+    );
+    app.use(
+        "/dynserv/", 
+        createProxyMiddleware({
+            target: `https://localhost:5000`,
+            changeOrigin: true,
+            secure: false
+        })
+    );
+
+    app.use(morgan("combined"));
 };
+
